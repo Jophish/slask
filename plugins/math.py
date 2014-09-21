47eaa6361383fcab9e95c3c7310fd43a1b46ce7e
joe <jophish126@gmail.com>
2014-09-21 19:09:45 -0400

    added math module

---
+++ plugins/math.py
@@ -0,0 +1,18 @@
+"""!math <mathstuffs> evaluates to mathstuffs"""
+
+from urllib import quote
+import re
+import requests
+from random import shuffle
+
+def math(maths):
+    maths.replace("^","**")
+    return(eval(maths))
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!math (.*)", text)
+    if not match: return
+
+    
+    return math(math)
