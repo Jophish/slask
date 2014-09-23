ba334b353cb33a74ca8d49b593f62d8b5a609c7e
joe <jophish126@gmail.com>
2014-09-22 20:20:49 -0400

    added feeds.py

---
+++ feeds.py
@@ -0,0 +1,3 @@
+links = ['http://rss.nytimes.com/services/xml/rss/nyt/World.xml', 'http://www.npr.org/rss/rss.php?id=1008']
+usernames = ["NY Times: World News", "NPR: Arts & Culture"]
+icons = ["http://ryanmartin.me/wp-content/uploads/2014/05/Elkhart-Truth-new-website-favicon.png", "https://yt3.ggpht.com/-sLLAakJX6W4/AAAAAAAAAAI/AAAAAAAAAAA/vTFRnbObrG4/s900-c-k-no/photo.jpg"]
