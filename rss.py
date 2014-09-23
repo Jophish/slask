bf3ef6f570532d7de579af07daacbf20e448ac03
joe <jophish126@gmail.com>
2014-09-22 20:19:52 -0400

    added scaling to rss bot

--- rss.py
+++ rss.py
@@ -3,38 +3,48 @@ import requests
 import json
 import time
 import bs4
+import feeds
 
 d = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/World.xml')
 
-
 link = "https://shfriends.slack.com/services/hooks/incoming-webhook?token=IaEYnLmPQCYxdoQypHnK52Al"
 
+links = feeds.links
+usernames = feeds.usernames
+icons = feeds.icons
 
+currentArticles = [None]*len(links)
+recentArticles = [None]*len(links)
 
-recentArticle = None
+check = True #enable if you don't want the bot spamming the channel with rss feeds when it's turned on
 
 while True:
 
-	d = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/World.xml')
+	for x in list(range(len(links))):
 
-	currentArticle = d['entries'][0]['title']
+		d = feedparser.parse(links[x])
 
-	if str(recentArticle) != str(currentArticle):
+		currentArticles[x] = d['entries'][0]['title']
 
-		recentArticle = str(d['entries'][0]['title'])
+		if str(recentArticles[x]) != str(currentArticles[x]):
+
+			recentArticles[x] = str(d['entries'][0]['title'])
 		
-		session = requests.session()
+			session = requests.session()
 
-		req = session.get('http://tinyurl.com/api-create.php?url='+d['entries'][0]['link'])
+			req = session.get('http://tinyurl.com/api-create.php?url='+d['entries'][0]['link'])
 
-		doc =  str(bs4.BeautifulSoup(req.content)) #[15:-18]
-
-
-		payload = json.JSONEncoder().encode({"username": "NY Times", "icon_url" : "http://ryanmartin.me/wp-content/uploads/2014/05/Elkhart-Truth-new-website-favicon.png", "attachments" : [{"title": d['entries'][0]['title'], "text": '\n'+d['entries'][0]['summary_detail']['value'][:d['entries'][0]['summary_detail']['value'].index('<')] +'\n' + str(doc)}, ]})
-
-		requests.post(link, payload)
+			doc =  str(bs4.BeautifulSoup(req.content)) [15:-18]
 
 
+			payload = json.JSONEncoder().encode({"username": usernames[x], "icon_url" : icons[x], "attachments" : [{"title": d['entries'][0]['title'], "text": '\n'+d['entries'][0]['summary_detail']['value'][:d['entries'][0]['summary_detail']['value'].index('<')] +'\n' + str(doc)}, ]})
 
-	time.sleep(1)
+			if check == True:
+				check = False
+			else: 
+				requests.post(link, payload)
+
+
+
+		time.sleep(1)
 
